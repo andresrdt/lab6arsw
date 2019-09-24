b75ef7e8dc3156a71f203b0ab1010eae64d0f297
andresrdt <andres.rodriguez-de@mail.escuelaing.edu.co>
2019-09-24 17:48:45 -0500

    apimock

---
+++ src/main/resources/static/apimock.js
@@ -0,0 +1,49 @@
+var apimok = (function () {
+
+    var mockdata = [];
+
+        mockdata["JhonConnor"] = [
+        {
+            author: "JhonConnor",
+            name: "house",
+            points: [
+                {
+                    x: 10,
+                    y: 20
+                },
+                {
+                    x: 15,
+                    y: 25
+                }
+            ]
+        },
+        {
+            author: "JhonConnor",
+            name: "gear",
+            points: [
+                {
+                    x: 30,
+                    y: 35
+                },
+                {
+                    x: 40,
+                    y: 45
+                }
+            ]
+        }
+    ];
+
+    return {
+        getBlueprintsByAuthor:function(name, callback) {
+            callback(
+                mockdata[name]
+            )
+        },
+        getBlueprintsByNameAndAuthor:function(autor,obra,callback){
+            callback(
+                mockdata[autor].filter(temporal => {return temporal.name === obra;})[0]
+            )
+        }
+    }
+
+})();
