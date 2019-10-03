6b07cbc1a2c4eb956493758ed1a9daa64ab7d9c9
Nicolas patiño <nicolas.patino@mail.escuelaing.edu.co>
2019-10-03 18:55:42 -0500

    hola

--- src/main/resources/static/apiclient.js
+++ src/main/resources/static/apiclient.js
@@ -1,24 +1,12 @@
 const apiUrl = "http://localhost:8080/blueprints/"
 var ApiClient = (function() {
 
-    getBlueprintsByAuthor = function(author, callback) {
+    var getBlueprintsByAuthor = function(author, callback) {
         var blueprints = $.get("http://localhost:8080/blueprints/" + author, function(data) {
             return data;
         });
         return callback(null, blueprints);
-    
-    getBlueprintsByNameAndAuthor: function(author, name, callback) {
-      jQuery.ajax({
-        url: apiUrl + author + "/" + name,
-        success: function(result) {
-          callback(result);
-        },
-        async: true
-      });
     }
-<<<<<<< HEAD
-})();    
-=======
     var getBlueprintsByNameAndAuthor=function(author,name,callback){
           var blueprint = $.get("http://localhost:8080/blueprints/" + author+"/"+name, function(data) {
                 return data;
@@ -28,6 +16,5 @@ var ApiClient = (function() {
     return {
         getBlueprintsByAuthor: getBlueprintsByNameAndAuthor
     }
-    
+
 })();
->>>>>>> 7aee609ebca22fea6189f1eaa627610abc378502
